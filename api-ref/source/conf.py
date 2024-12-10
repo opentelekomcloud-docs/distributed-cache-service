44b044ce735455f37276acaedd550213896ba532
Sebastian Gode <sebastian.gode@telekom.de>
2024-12-10 12:30:48 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -110,7 +110,7 @@ html_copy_source = False
 latex_documents = [
     ('index',
      'dcs-api-ref.tex',
-     u'Distributed Cache Service - API Reference',
+    u'Distributed Cache Service - API Reference',
      u'OpenTelekomCloud', 'manual'),
 ]
 
